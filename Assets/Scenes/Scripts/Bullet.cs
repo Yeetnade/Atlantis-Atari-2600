be655c84beb802ff25b45fd70dd093abc12c73d0
Yeetnade <93275338+Yeetnade@users.noreply.github.com>
2022-06-11 21:18:52 -0700

    All art assets created

---
+++ Assets/Scenes/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb2;
+
+
+    // Update is called once per frame
+    void Start()
+    {
+        rb2.velocity = transform.up * speed;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scenes/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public Transform firePoint2;
+    public Transform firePoint3;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if(Input.GetKey("right"))
+            {
+                Instantiate(bulletPrefab, firePoint2.position, firePoint2.rotation);
+            }
+            else if(Input.GetKey("left"))
+            {
+                Instantiate(bulletPrefab, firePoint3.position, firePoint3.rotation);
+            }
+            else
+            {
+                Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            }
+        }
+
+    }
+}
